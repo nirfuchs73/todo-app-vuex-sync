6e5afb1a25287dea0496b01e548d8ac2f3974d41
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-12 09:19:42 +0200

    Update edit

--- pages/TodoEdit.js
+++ pages/TodoEdit.js
@@ -6,17 +6,11 @@ export default {
         <section v-if="todo">
             <h1>{{(todo._id)? 'Todo Edit' : 'Todo Add'}}</h1>
             <form v-on:submit.prevent="saveTodo" class="todo-edit flex">
-                <div class="flex"> 
-                    <label>Text:</label>
-                    <input type="text" v-model="todo.txt" />
-                </div>
-                <div class="flex"> 
-                    <label>Importance:</label>
-                    <input type="text" v-model="todo.importance" />
-                </div>
-                <div class="flex"> 
-                    <button type="submit">Save</button>
-                </div>
+                <label>Text:</label>
+                <input type="text" v-model="todo.txt" />
+                <label>Importance:</label>
+                <input type="text" v-model="todo.importance" />
+                <button type="submit">Save</button>
             </form>
         </section>  
 
